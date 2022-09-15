48669d7cf8c414d983272f629ae2dbafd0543e25
David Ilie <david@davidilie.com>
2022-09-15 21:13:00 +0100

    added prisma export

--- packages/server/backend.ts
+++ packages/server/backend.ts
@@ -1,3 +1,6 @@
 export { default } from "./src/auth";
 export { appRouter } from "./src/api";
 export { createContext } from "./src/api/context";
+
+import prismaClient from "./src/prisma";
+export const prisma = prismaClient;
